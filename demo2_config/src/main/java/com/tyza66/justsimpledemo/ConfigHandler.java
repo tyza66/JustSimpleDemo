28ea105768f6412c17a247484067f9a94d5750a0
孙达明 <shun_@outlook.com>
2025-03-19 15:51:06 +0800

    feat:更新配置介绍

--- demo2_config/src/main/java/com/tyza66/justsimpledemo/ConfigHandler.java
+++ demo2_config/src/main/java/com/tyza66/justsimpledemo/ConfigHandler.java
@@ -8,12 +8,15 @@ import org.mutantcat.justsimple.instance.InstanceHandler;
 public class ConfigHandler {
     public static String[] doConfig(String[] args) {
         // 配置信息 可以存入一个配置类的 当然这个配置实例也可以使用@Instance注解自动注册
-        CorsConfig corsConfig = CorsConfigBuilder.forAnyOrigin() // 允许任意来源
-                .allowNullOrigin() // 允许空的来源
-                .allowedRequestMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE) // 允许的方法
-                .allowedRequestHeaders("*") // 允许任意请求头
-                .exposeHeaders("X-Custom-Header") // 暴露自定义的响应头
-                .maxAge(3600) // 设置预检请求的缓存时间（单位秒）
+//        CorsConfig corsConfig = CorsConfigBuilder.forAnyOrigin() // 允许任意来源
+//                .allowNullOrigin() // 允许空的来源
+//                .allowedRequestMethods(HttpMethod.GET) // 允许的方法
+//                .allowedRequestHeaders("*") // 允许任意请求头
+//                .exposeHeaders("X-Custom-Header") // 暴露自定义的响应头
+//                .maxAge(3600) // 设置预检请求的缓存时间（单位秒）
+//                .build();
+        CorsConfig corsConfig = CorsConfig.withAnyOrigin()
+                .allowedRequestMethods(HttpMethod.GET) // 只允许 GET 请求
                 .build();
         InstanceHandler.putInstance("CorsConfigInstanceName",corsConfig);
 
--- demo2_config/src/main/java/com/tyza66/justsimpledemo/instance/CorsConfig.java
+++ demo2_config/src/main/java/com/tyza66/justsimpledemo/instance/CorsConfig.java
@@ -5,17 +5,17 @@ import io.netty.handler.codec.http.cors.CorsConfigBuilder;
 import org.mutantcat.justsimple.annotation.Instance;
 import org.mutantcat.justsimple.instance.InstanceHandler;
 
-@Instance
+//@Instance
 public class CorsConfig {
 
-    public CorsConfig() {
-        // 这样调用实例的构造方法，会将配置实例放入实例池中
-        InstanceHandler.putInstance("CorsConfigInstanceName",  CorsConfigBuilder.forAnyOrigin() // 允许任意来源
-                .allowNullOrigin() // 允许空的来源
-                .allowedRequestMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE) // 允许的方法
-                .allowedRequestHeaders("*") // 允许任意请求头
-                .exposeHeaders("X-Custom-Header") // 暴露自定义的响应头
-                .maxAge(3600) // 设置预检请求的缓存时间（单位秒）
-                .build());
-    }
+//    public CorsConfig() {
+//        // 这样调用实例的构造方法，会将配置实例放入实例池中
+//        InstanceHandler.putInstance("CorsConfigInstanceName",  CorsConfigBuilder.forAnyOrigin() // 允许任意来源
+//                .allowNullOrigin() // 允许空的来源
+//                .allowedRequestMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE) // 允许的方法
+//                .allowedRequestHeaders("*") // 允许任意请求头
+//                .exposeHeaders("X-Custom-Header") // 暴露自定义的响应头
+//                .maxAge(3600) // 设置预检请求的缓存时间（单位秒）
+//                .build());
+//    }
 }
